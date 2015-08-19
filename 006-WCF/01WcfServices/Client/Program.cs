332b338a56f0210e677df6c65eb1c8e3485d9271
liqipeng <liqipeng127@gmail.com>
2015-08-19 20:39:40 +0800

    first wcf example in WCF全面解析1.[补充1]

--- 006-WCF/01WcfServices/Client/Program.cs
+++ 006-WCF/01WcfServices/Client/Program.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +14,36 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            string serviceUrl = "http://127.0.0.1:23232/calculatorservice";
+            //WithoutHeader();
+            WithHeader();
+
+            Console.Read();
+        }
+
+        static void WithoutHeader() 
+        {
+            string serviceUrl = "http://127.0.0.1:23232/myservices/calculatorservice";
 
             using (ChannelFactory<ICalculator> channelFactory = new ChannelFactory<ICalculator>(new WSHttpBinding(), serviceUrl))
+            {
+                ICalculator proxy = channelFactory.CreateChannel();
+                Console.WriteLine("x + y = {2} when x = {0} and y = {1}", 1, 2, proxy.Add(1, 2));
+                Console.WriteLine("x - y = {2} when x = {0} and y = {1}", 1, 2, proxy.Substract(1, 2));
+                Console.WriteLine("x * y = {2} when x = {0} and y = {1}", 1, 2, proxy.Multiply(1, 2));
+                Console.WriteLine("x / y = {2} when x = {0} and y = {1}", 1, 2, proxy.Divide(1, 2));
+            }
+        }
+
+        static void WithHeader() 
+        {
+            AddressHeader header = AddressHeader.CreateAddressHeader("UserType", "http://www.aaa.com", "Licensed User");
+            EndpointAddress address = new EndpointAddress(new Uri("http://127.0.0.1:23232/myservices/calculatorservice"), header);
+            //EndpointAddress address = new EndpointAddress(new Uri("http://127.0.0.1:23232/myservices/calculatorservice"));
+            Binding httpBinding = new WS2007HttpBinding();
+            ContractDescription contract = ContractDescription.GetContract(typeof(ICalculator));
+            ServiceEndpoint endPoint = new ServiceEndpoint(contract, httpBinding, address);
+
+            using (ChannelFactory<ICalculator> channelFactory = new ChannelFactory<ICalculator>(endPoint))
             {
                 ICalculator proxy = channelFactory.CreateChannel();
                 Console.WriteLine("x + y = {2} when x = {0} and y = {1}", 1, 2, proxy.Add(1, 2));
--- 006-WCF/01WcfServices/Hosting/Program.cs
+++ 006-WCF/01WcfServices/Hosting/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,25 +15,102 @@ namespace Hosting
     {
         static void Main(string[] args)
         {
-            string serviceUrl = "http://127.0.0.1:23232/calculatorservice";
+            //UseFullAddress();
+            //UseBaseAddress();
+            UseAddressHeader();
 
-            using (ServiceHost serviceHost = new ServiceHost(typeof(CalculatorService))) 
+        }
+
+        /// <summary>
+        /// 使用完整地址
+        /// </summary>
+        static void UseFullAddress() 
+        {
+            string serviceUrl = "http://127.0.0.1:23232/myservices/calculatorservice";
+
+            using (ServiceHost serviceHost = new ServiceHost(typeof(CalculatorService)))
             {
-                serviceHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "http://127.0.0.1:23232/calculatorservice");
-                if (serviceHost.Description.Behaviors.Find<ServiceMetadataBehavior>() == null) 
+                serviceHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "http://127.0.0.1:23232/myservices/calculatorservice");
+
+                if (serviceHost.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
                 {
                     ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
                     behavior.HttpGetEnabled = true;
                     behavior.HttpGetUrl = new Uri(string.Format("{0}{1}", serviceUrl, "metadata"));
+                    serviceHost.Description.Behaviors.Add(behavior);
                 }
-                serviceHost.Opened += delegate{
+                serviceHost.Opened += delegate
+                {
                     Console.WriteLine("CalculatorService is opened, press any key to end service.");
                 };
                 serviceHost.Open();
 
                 Console.Read();
             }
+        }
 
+        /// <summary>
+        /// 使用基地址
+        /// </summary>
+        static void UseBaseAddress() 
+        {
+            Uri[] baseAddresses = new Uri[2];
+            baseAddresses[0] = new Uri("http://127.0.0.1:23232/myservices");
+            baseAddresses[1] = new Uri("net.tcp://127.0.0.1:23233/myservices");
+
+            using (ServiceHost serviceHost = new ServiceHost(typeof(CalculatorService), baseAddresses))
+            {
+                serviceHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "calculatorservice");
+                serviceHost.AddServiceEndpoint(typeof(ICalculator), new NetTcpBinding(), "calculatorservice");
+
+                if (serviceHost.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
+                {
+                    ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+                    behavior.HttpGetEnabled = true;
+                    behavior.HttpGetUrl = new Uri(string.Format("{0}{1}", baseAddresses[0].AbsoluteUri, "/calculatorservice/metadata"));
+                    serviceHost.Description.Behaviors.Add(behavior);
+                }
+
+                serviceHost.Opened += delegate
+                {
+                    Console.WriteLine("CalculatorService is opened, press any key to end service.");
+                };
+                serviceHost.Open();
+
+                Console.Read();
+            }
+        }
+
+        static void UseAddressHeader() 
+        {
+            using (ServiceHost serviceHost = new ServiceHost(typeof(CalculatorService)))
+            {
+                AddressHeader header = AddressHeader.CreateAddressHeader("UserType", "http://www.aaa.com", "Licensed User");
+                EndpointAddress address = new EndpointAddress(new Uri("http://127.0.0.1:23232/myservices/calculatorservice"), header);
+                //EndpointAddress address = new EndpointAddress(new Uri("http://127.0.0.1:23232/myservices/calculatorservice"));
+
+                Binding httpBinding = new WS2007HttpBinding();
+                ContractDescription contract = ContractDescription.GetContract(typeof(ICalculator));
+                ServiceEndpoint endPoint = new ServiceEndpoint(contract, httpBinding, address);
+
+                serviceHost.AddServiceEndpoint(endPoint);
+
+                if (serviceHost.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
+                {
+                    ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+                    behavior.HttpGetEnabled = true;
+                    behavior.HttpGetUrl = new Uri("http://127.0.0.1:23232/myservices/calculatorservice/metadata");
+                    serviceHost.Description.Behaviors.Add(behavior);
+                }
+
+                serviceHost.Opened += delegate
+                {
+                    Console.WriteLine("CalculatorService is opened, press any key to end service.");
+                };
+                serviceHost.Open();
+
+                Console.Read();
+            }
         }
     }
 }
