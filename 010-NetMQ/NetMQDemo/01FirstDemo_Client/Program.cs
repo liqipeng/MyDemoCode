e8b48e9d34384da1fb9f0bcbdf7c0992abc0f242
liqipeng <liqipeng127@gmail.com>
2015-12-09 01:45:28 +0800

    add NetMQ demo

---
+++ 010-NetMQ/NetMQDemo/01FirstDemo_Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using NetMQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01FirstDemo_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NetMQContext context = NetMQContext.Create())
+            {
+                Client(context);
+            }
+        }
+
+        static void Client(NetMQContext context)
+        {
+            using (NetMQSocket clientSocket = context.CreateRequestSocket())
+            {
+                clientSocket.Connect("tcp://127.0.0.1:5555");
+
+                while (true)
+                {
+                    Console.WriteLine("Please enter your message:");
+                    string message = Console.ReadLine();
+                    clientSocket.SendFrame(message);
+
+                    string answer = clientSocket.ReceiveFrameString();
+
+                    Console.WriteLine("Answer from server: {0}", answer);
+
+                    if (message == "exit")
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 010-NetMQ/NetMQDemo/01FirstDemo_Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using NetMQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01FirstDemo_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NetMQContext context = NetMQContext.Create())
+            {
+                Server(context);
+            }
+        }
+
+        static void Server(NetMQContext context)
+        {
+            using (NetMQSocket serverSocket = context.CreateResponseSocket())
+            {
+                serverSocket.Bind("tcp://*:5555");
+
+                while (true)
+                {
+                    string message = serverSocket.ReceiveFrameString();
+
+                    Console.WriteLine("Receive message {0}", message);
+
+                    serverSocket.SendFrame("World");
+
+                    if (message == "exit")
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
